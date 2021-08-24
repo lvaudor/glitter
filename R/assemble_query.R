f783d8a9ce732f1254e56360e711e21850d2b408
lvaudor <lise.vaudor@ens-lyon.fr>
2021-08-24 10:58:57 +0200

    add filtering

--- R/assemble_query.R
+++ R/assemble_query.R
@@ -4,14 +4,15 @@
 #' @param query_parts a list with elements of the query
 #' @export
 #' @examples
-#' query=add_triplets(subject="?city",verb="wdt:P31",object="wd:Q515", limit=10) %>%
-#' add_triplets(subject="?city",verb="wdt:P1082",object="?pop") %>%
+#' add_triplets(subject="?city",verb="wdt:P31",object="wd:Q515", limit=10) %>%
+#' add_triplets(subject="?city",verb="wdt:P1082",object="?pop", label="?city", language="en") %>%
 #' build_sparql() %>%
 #' send_sparql()
 build_sparql=function(query_parts){
   query=paste0("SELECT ", paste0(query_parts$select,collapse=" "),"\n",
                "WHERE{\n",
                query_parts$body,"\n",
+               query_parts$filter,"\n",
                query_parts$service,"\n",
                "}\n",
                query_parts$limit)
@@ -25,11 +26,13 @@ build_sparql=function(query_parts){
 #' @param optional whether the triplet is optional (other results are sent back even if this particular triplet is missing)
 #' @param label a vector of variables for which to include a label column (defaults to NA)
 #' @param limit the max number of items sent back
+#' @param filter the filters to apply
 #' @param within_box if provided, rectangular bounding box for the triplet query.
 #' Provided as list(southwest=c(long=...,lat=...),northeast=c(long=...,lat=...))
 #' @param within_distance if provided, circular bounding box for the triplet query.
 #' Provided as list(center=c(long=...,lat=...), radius=...), with radius in kilometers.
 #' The center can also be provided as a variable (for instance, "?location") for the center coordinates to be retrieved directly from the query.
+#' @param language the language in which the labels will be provided (defaults to "en" for English)
 #' @export
 #' @examples
 #' add_triplets(subject="?city",
@@ -46,13 +49,18 @@ build_sparql=function(query_parts){
 #' northeast=c(7,47))) %>%
 #' build_sparql() %>%
 #' send_sparql()
-add_triplets=function(query=NA,subject,verb,object,optional=FALSE, label=NA, limit=NA,
+add_triplets=function(query=NA,subject,verb,object,
+                      optional=FALSE, label=NA,
+                      filter=NA,
+                      limit=NA,
                       within_box=c(NA,NA),
-                      within_distance=c(NA,NA)){
+                      within_distance=c(NA,NA),
+                      language="en"){
   query_parts=list(select=build_part_select(query,subject,verb,object,label),
                    body=build_part_body(query,subject,verb,object,optional,
                                         within_box=within_box, within_distance=within_distance),
-                   service=build_part_service(query),
+                   service=build_part_service(query, language),
+                   filter=build_part_filter(query, filter),
                    limit=build_part_limit(query,limit))
   return(query_parts)
 }
