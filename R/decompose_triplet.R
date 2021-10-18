0b29ae6edc2835dd99ad11719d6cfaf831043797
lvaudor <lise.vaudor@ens-lyon.fr>
2021-10-18 17:35:49 +0200

    reorganise if-elses

--- R/decompose_triplet.R
+++ R/decompose_triplet.R
@@ -4,17 +4,21 @@
 #' @param verb the property (for instance "wdt:P190")
 #' @param object an anonymous variable (for instance, and by default, "?object") or item (for instance "wd:Q456"))
 decompose_triplet=function(triplet, subject,verb,object){
+  # decompose triplet if necessary: get three elements subject-verb-object as a list
   if(!is.null(triplet)){
-    elements=stringr::str_split(triplet,"\\s") %>%
-      unlist()
-    result=list(subject=elements[1],
-                verb=elements[2],
-                object=elements[3])
-    return(result)
-  }else{
-    result=list(subject=subject,
-                verb=verb,
-                object=object)
-    return(result)
+    elements=stringr::str_split(triplet,"\\s") %>% unlist()
+    elements=list(subject=elements[1],
+                  verb=elements[2],
+                  object=elements[3])
+    }else{
+    elements=list(subject=subject,
+                  verb=verb,
+                  object=object)
   }
+  # tests for syntax error in subject verb and object
+  elements_correct=purrr::map_lgl(elements,is_svo_correct)
+  if(!all(elements_correct)){
+      stop("At least one element among subject, verb or object is incorrectly stated.")
+  }
+  return(elements)
 }
