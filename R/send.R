46bb9fb23d178031010268473b2b71bd646c7a87
lvaudor <lise.vaudor@ens-lyon.fr>
2021-10-20 15:58:59 +0200

    query_parts => query

--- R/send.R
+++ R/send.R
@@ -1,5 +1,5 @@
 #' Assemble query parts into a sparql query and send it to endpoint to get a tibble as a result.
-#' @param query_parts a list with elements of the query
+#' @param query a list with elements of the query
 #' @param endpoint a string or url corresponding to a SPARQL endpoint. Defaults to "Wikidata"
 #' @export
 #' @examples
@@ -7,8 +7,8 @@
 #' add_triplets(subject="?city",verb="wdt:P1082",object="?pop", label="?city", language="en") %>%
 #' spq_head(n=5) %>%
 #' send()
-send=function(query_parts, endpoint="Wikidata"){
-  sparql_query=build_sparql(query_parts=query_parts,endpoint=endpoint)
+send=function(query, endpoint="Wikidata"){
+  sparql_query=build_sparql(query=query,endpoint=endpoint)
   result=send_sparql(sparql_query, endpoint=endpoint)
   return(result)
 }
