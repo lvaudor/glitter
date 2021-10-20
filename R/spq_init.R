c2f097b4a5eccb5e165e8795d30e9deaa79328ba
lvaudor <lise.vaudor@ens-lyon.fr>
2021-10-20 17:49:02 +0200

    add spq_init()

---
+++ R/spq_init.R
@@ -0,0 +1,16 @@
+#' Initialize a query object.
+#' @export
+#' @example query_init()
+spq_init=function(){
+    query=list(prefixes=tibble(name=NULL,url=NULL),
+               prefixed=NULL,
+               previous_subject=NULL,
+               select=NULL,
+               body="",
+               service="",
+               filter="",
+               limit="",
+               group_by="",
+               order_by="")
+    return(query)
+}
