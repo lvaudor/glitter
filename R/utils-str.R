2a4ca5c2a838534f43620e27f281638e5d3a6cb0
lvaudor <lise.vaudor@ens-lyon.fr>
2021-10-20 16:01:04 +0200

    better indications / pbs with triplet syntax

--- R/utils-str.R
+++ R/utils-str.R
@@ -10,36 +10,64 @@ is_variable=function(string){
 #' checks whether subject/verb/object is an item
 #' @param string
 #' @examples
-#' glitter:::is_uri("elem") #returns FALSE
-#' glitter:::is_uri("wd:Q456") #returns TRUE
-#' glitter:::is_uri("<http://qsdfqsdfsqdf>") #returns TRUE
-is_uri=function(string){
-  if(stringr::str_detect(string,"^<http.*>$")){return(TRUE)}
+#' glitter:::is_prefixed("elem") #returns FALSE
+#' glitter:::is_prefixed("wd:Q456") #returns TRUE
+#' glitter:::is_prefixed("<http://qsdfqsdfsqdf>") #returns TRUE
+is_prefixed=function(string){
+  if(stringr::str_detect(string,"^<http.*>$")){return(FALSE)}
   if(stringr::str_detect(string,":")){
     return(TRUE)
   }
   return(FALSE)
 }
 
-#' transforms paths into uris
+#' checks whether string is a url
 #' @param string
 #' @examples
-#' glitter:::as_uri("wdt:P31/wdt:P279*")
-#' glitter:::as_uri("?item")
-as_uri=function(string){
-  if(is_uri(string)){
-    result=string
-    if(stringr::str_detect(string,"\\/")){
-      result=stringr::str_split(string,"\\/") %>% unlist()
-    }
-  }else{
-    result=NULL
-  }
-  return(result)
+#' is_url("<http://qsdqsdfqsdfqs.html>")
+is_url=function(string){
+  if(stringr::str_detect(string,"^<http.*>$")){return(TRUE)}
+  return(FALSE)
 }
 
 
-is_uri_correct=function(string,prefixes="",endpoint="Wikidata"){
+
+#' checks whether string is a value
+#' @param string
+#' @examples
+#' glitter:::is_value("'blabla'") #TRUE
+#' glitter:::is_value('"blabla"') #TRUE
+#' glitter:::is_value('blabla') #FALSE
+is_value=function(string){
+  if(stringr::str_detect(string,"^[\'\"].+[\'\"].*$")){
+  return(TRUE)}
+  return(FALSE)
+}
+
+#' transforms paths into uris
+#' @param string
+#' @examples
+#' glitter:::keep_prefixed("wdt:P31/wdt:P279*") # wdt:P31 wdt:P279*
+#' glitter:::keep_prefixed("?item") # NULL
+#' glitter:::keep_prefixed("<http://qdsfqsdfqsfqsdf.html>") # NULL
+#' glitter:::keep_prefixed("wd:P343") # "wd:P343"
+#' glitter:::keep_prefixed("'11992081'^^xsd:integer") # xsd:integer
+keep_prefixed=function(string){
+  if(is_prefixed(string)){
+    prefixed=string
+    if(stringr::str_detect(string,"\\/")){
+      prefixed=stringr::str_split(string,"\\/") %>% unlist()
+    }
+    prefixed=stringr::str_extract(prefixed,
+                                  "[^\\^]*$")
+  }else{
+    prefixed=NULL
+  }
+  return(prefixed)
+}
+
+
+is_prefix_correct=function(string,prefixes="",endpoint="Wikidata"){
   if(endpoint=="Wikidata"){
     prefixes=usual_prefixes %>% filter(type=="Wikidata")
   }
@@ -48,9 +76,10 @@ is_uri_correct=function(string,prefixes="",endpoint="Wikidata"){
     return(TRUE)
   }
   if(!(prefix %in% prefixes$name)){
+
     stop(glue::glue("prefix {prefix} is not known. Please provide it through function spq_prefix()."))
   }
-  return(is_uri(string))
+  return(is_prefixed(string))
 }
 
 
@@ -70,10 +99,17 @@ as_values=function(string){
 #' checks whether subject/verb/object is stated correctly
 #' @param string
 #' @examples
-#' glitter:::is_svo_correct("elem")
+#' glitter:::is_svo_correct("elem") #FALSE
+#' glitter:::is_svo_correct("a") #TRUE
+#' glitter:::is_svo_correct("is") #TRUE
+#' glitter:::is_svo_correct("'0000000121012885'") #TRUE
+#' glitter:::is_svo_correct(".") #TRUE
 is_svo_correct=function(string){
+  if(string %in% c(".", "a","is")){return(TRUE)}
   if(is_variable(string)){return(TRUE)}
-  if(is_uri(string)){return(TRUE)}
+  if(is_prefixed(string)){return(TRUE)}
+  if(is_url(string)){return(TRUE)}
+  if(is_value(string)){return(TRUE)}
   return(FALSE)
 }
 
