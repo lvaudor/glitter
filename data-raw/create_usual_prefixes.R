0a31d656879219afa133e74b9dfde57794178fdd
lvaudor <lise.vaudor@ens-lyon.fr>
2021-11-03 15:19:02 +0100

    update usual_prefixes

--- data-raw/create_usual_prefixes.R
+++ data-raw/create_usual_prefixes.R
@@ -2,10 +2,10 @@ usual_prefixes=readr::read_csv("data-raw/usual_prefixes.csv")
 usethis::use_data(usual_prefixes,overwrite=TRUE)
 
 # ogc:  http://www.opengis.net/
-#   geo:  http://www.opengis.net/ont/geosparql#
+# geo:  http://www.opengis.net/ont/geosparql#
 # geof: http://www.opengis.net/def/function/geosparql/
-#   geor: http://www.opengis.net/def/rule/geosparql/
-#   sf:  http://www.opengis.net/ont/sf#
+# geor: http://www.opengis.net/def/rule/geosparql/
+# sf:  http://www.opengis.net/ont/sf#
 # gml: http://www.opengis.net/ont/gml#
 # my:  http://example.org/ApplicationSchema
 
