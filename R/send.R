72a44b79bcee07b9eceb1c8a2d552e690725a0a8
lvaudor <lise.vaudor@ens-lyon.fr>
2021-11-15 16:40:11 +0100

    add spq_init() to all queries

--- R/send.R
+++ R/send.R
@@ -3,9 +3,11 @@
 #' @param endpoint a string or url corresponding to a SPARQL endpoint. Defaults to "Wikidata"
 #' @export
 #' @examples
+#' query=spq_init() %>%
 #' spq_add(subject="?city",verb="wdt:P31",object="wd:Q515") %>%
 #' spq_add(subject="?city",verb="wdt:P1082",object="?pop", label="?city", language="en") %>%
 #' spq_head(n=5) %>%
+#'
 #' send()
 send=function(query, endpoint="Wikidata"){
   sparql_query=build_sparql(query=query,endpoint=endpoint)
