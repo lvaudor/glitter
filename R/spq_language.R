2afdd28bc3bfcd59f952fecbc66db7c5f879a61f
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-02-18 16:20:40 +0100

    less long examples

--- R/spq_language.R
+++ R/spq_language.R
@@ -3,17 +3,15 @@
 #' @param language the language in which the labels will be provided (defaults to "en" for English). See complete list of Wikimedia language codes [here](https://www.wikidata.org/wiki/Help:Wikimedia_language_codes/lists/all). You can also set language to "auto" and then the Wikidata SPARQL engine will try and detect your language automatically. Specifying several languages will return labels with languages following the priority specified (e.g. with language="fr,en", the label will be returned preferentially in French, or, if there is not French label for the item, in English).
 #' @export
 #' @examples
-#' tib=spq_init() %>%
+#' spq_init() %>%
 #'  spq_add("?film wdt:P31 wd:Q11424", label="?film") %>%
 #'  spq_language("fr,en") %>%
-#'  spq_head(10) %>%
-#'  send()
+#'  spq_head(10)
 #'
-#' tib=spq_init() %>%
+#' spq_init() %>%
 #'  spq_add("?film wdt:P31 wd:Q11424", label="?film") %>%
 #'  spq_language("auto,es,it,de") %>%
-#'  spq_head(10) %>%
-#'  send()
+#'  spq_head(10)
 spq_language=function(query=NULL,
                       language="en"){
   language=stringr::str_replace(language,"auto","[AUTO_LANGUAGE]")
