11b3da6cbd06eaac874d33277698f3dbb9655d69
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-05-30 13:50:16 +0200

    add spq_count

--- R/spq_tally.R
+++ R/spq_tally.R
@@ -1,5 +1,7 @@
 #' Count the observations
 #'
+#' These functions are inspired by `dplyr::count()` and `dplyr::tally()`.
+#' `spq_tally()` assumes you've already done the grouping.
 #' @inheritParams spq_arrange
 #' @param sort If `TRUE`, will show the largest groups at the top. (like the `sort` argument
 #' of `dplyr::tally()`)
@@ -17,11 +19,28 @@
 #' spq_tally(name = "n_films") %>%
 #' spq_perform()
 #'
+#' # the same with spq_count
+#'
+#' spq_init() %>%
+#' spq_add("?film wdt:P31 wd:Q11424") %>%
+#' spq_mutate(narrative_location = wdt::P840(film), .label = "?narrative_location") %>%
+#' spq_count(name = "n_films") %>%
+#' spq_perform()
+#'
+#' # Now with grouping
 #' spq_init() %>%
 #' spq_add("?film wdt:P31 wd:Q11424",.label="?film") %>%
 #' spq_mutate(narrative_location = wdt::P840(film), .label = "?narrative_location") %>%
 #' spq_group_by(narrative_locationLabel) %>%
 #' spq_tally(sort = TRUE, name = "n_films") %>%
+#' spq_perform()
+#'
+#'
+#' # More direct with spq_count()
+#' spq_init() %>%
+#' spq_add("?film wdt:P31 wd:Q11424",.label="?film") %>%
+#' spq_mutate(narrative_location = wdt::P840(film), .label = "?narrative_location") %>%
+#' spq_count(narrative_locationLabel, sort = TRUE, name = "n_films") %>%
 #' spq_perform()
 #'}
 #' @export
@@ -38,4 +57,16 @@ spq_tally = function(.query, sort = FALSE, name = "n") {
   }
 
   .query
+}
+
+#' @rdname spq_tally
+#' @inheritParams spq_group_by
+#' @export
+spq_count = function(.query, ..., sort = FALSE, name = "n") {
+
+  if (length(rlang::enquos(...)) > 0) {
+    .query <- spq_group_by(.query, ...)
+  }
+
+    spq_tally(.query, sort = sort, name = name)
 }
--- tests/testthat/_snaps/spq_tally.md
+++ tests/testthat/_snaps/spq_tally.md
@@ -35,3 +35,40 @@
       GROUP BY ?narrative_locationLabel
       ORDER BY DESC(?n_films)
 
+# spq_count() works
+
+    Code
+      spq_init() %>% spq_add("?film wdt:P31 wd:Q11424") %>% spq_mutate(
+        narrative_location = wdt::P840(film), .label = "?narrative_location") %>%
+        spq_count()
+    Output
+      
+      SELECT (COUNT(*) AS ?n)
+      WHERE{
+      
+      ?film wdt:P31 wd:Q11424.
+      ?film wdt:P840 ?narrative_location.
+      
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "en".}
+      }
+      
+
+---
+
+    Code
+      spq_init() %>% spq_add("?film wdt:P31 wd:Q11424", .label = "?film") %>%
+        spq_mutate(narrative_location = wdt::P840(film), .label = "?narrative_location") %>%
+        spq_count(narrative_locationLabel, sort = TRUE, name = "n_films")
+    Output
+      
+      SELECT (COUNT(*) AS ?n_films) ?narrative_locationLabel
+      WHERE{
+      
+      ?film wdt:P31 wd:Q11424.
+      ?film wdt:P840 ?narrative_location.
+      
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "en".}
+      }
+      GROUP BY ?narrative_locationLabel
+      ORDER BY DESC(?n_films)
+
