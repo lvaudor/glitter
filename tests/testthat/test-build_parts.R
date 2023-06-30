007f1f1dd0dc973838894bdf46abd3e8be57a021
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-06-30 09:58:55 +0200

    fix test expect

--- tests/testthat/test-build_parts.R
+++ tests/testthat/test-build_parts.R
@@ -1,9 +1,9 @@
 test_that("build_part_xxx() return strings", {
 
-  x= glitter:::build_part_select(query=NULL,subject="?city",verb="wdt:P625",object="?coords", label="?city")
-  expect_true(is.character(x))
+  x = glitter:::build_part_select(query=NULL,subject="?city",verb="wdt:P625",object="?coords", label="?city")
+  expect_type(x, "character")
 
-  x= glitter:::build_part_body(query=NULL,subject="?city",verb="wdt:P625",object="?coords")
-  expect_true(is.character(x))
+  x = glitter:::build_part_body(query=NULL,subject="?city",verb="wdt:P625",object="?coords")
+  expect_type(x, "character")
 
 })
