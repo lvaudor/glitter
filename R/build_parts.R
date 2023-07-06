e7dee7be23a4e904755c0c95326ce419cec9292a
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-07-06 13:41:15 +0200

    rm unused function

--- R/build_parts.R
+++ R/build_parts.R
@@ -73,30 +73,3 @@ build_part_body = function(query = NA,
 
   glue::glue("{part_body}\n{new_triple}")
 }
-
-#' Builds the "select" part of a query.
-#' @param query a list with elements of the query
-#' @param subject an anonymous variable (for instance, and by default, "?subject") or item (for instance "wd:Q456"))
-#' @param verb the property (for instance "wdt:P190")
-#' @param object an anonymous variable (for instance, and by default, "?object") or item (for instance "wd:Q456"))
-#' @param label whether to get the label associated with the mentioned item
-#' @noRd
-build_part_select = function(query = NULL,
-                             subject = NULL,
-                             verb = NULL,
-                             object = NULL,
-                             label = NA) {
-
-  part_select = query[["select"]]
-
-  for (element in list(subject,verb,object)) {
-    if (is_variable(element)) {
-      part_select = c(part_select, element)
-      if (element %in% label) {
-        part_select = c(part_select, paste0(element,"Label"))
-      }
-    }
-  }
-
-  unique(part_select)
-}
--- tests/testthat/test-build_parts.R
+++ tests/testthat/test-build_parts.R
@@ -1,7 +1,4 @@
-test_that("build_part_xxx() return strings", {
-
-  x = glitter:::build_part_select(query=NULL,subject="?city",verb="wdt:P625",object="?coords", label="?city")
-  expect_type(x, "character")
+test_that("build_part_body() return strings", {
 
   x = glitter:::build_part_body(query=NULL,subject="?city",verb="wdt:P625",object="?coords")
   expect_type(x, "character")
