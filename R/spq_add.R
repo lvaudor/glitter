1805fbdf23a7e3890cefe5b1780e55c2a3d4ca16
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-07-19 14:45:14 +0200

    update example

--- R/spq_add.R
+++ R/spq_add.R
@@ -18,7 +18,8 @@
 #' @examples
 #' # find the cities
 #' spq_init() %>%
-#'   spq_add("?city wdt:P31/wdt:P279* wd:Q486972", .label="?city") %>%
+#'   spq_add("?city wdt:P31/wdt:P279* wd:Q486972") %>%
+#'   spq_label(city) %>%
 #'   spq_mutate(coords = wdt::P625(city),
 #'           .within_distance=list(center=c(long=4.84,lat=45.76),
 #'                                radius=5)) %>%
