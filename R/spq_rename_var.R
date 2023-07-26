69e3e933b64f63af54d15e015903c9b49978382b
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-07-26 14:23:39 +0200

    obvious

--- R/spq_rename_var.R
+++ R/spq_rename_var.R
@@ -1,10 +1,9 @@
 spq_rename_var <- function(.query, old, new) {
-  # error if the old variable name is not in vars
+
   if (!(question_mark(old) %in% .query[["vars"]][["name"]])) {
     cli::cli_abort("Can't rename {.field {old}} as it's not present in the query object.")
   }
 
-  # error if the new variable name is in vars
   if (question_mark(new) %in% .query[["vars"]][["name"]]) {
     cli::cli_abort("Can't rename {.field {old}} to {.field {new}} as {.field {new}} already exists.")
   }
