5ae4d921c72aa849ab291d002b61adc7ccf02923
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-09-21 12:43:42 +0200

    fix: fix transform_wikidata_coords()
    Fix example
    Fix #179
    
    Add test

--- R/coords.R
+++ R/coords.R
@@ -5,38 +5,38 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' query='SELECT ?item ?itemLabel ?coords ?date
-#' {
-#'   ?item wdt:P361 wd:Q1552;
-#'         wdt:P625 ?coords.
-#'   OPTIONAL{?item wdt:P1619 ?date.}
-#'   SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
-#' } ORDER BY ?itemLabel
-#' '
-#' tib=send_sparql(query)
-#' transform_wikidata_coords(tib, "coords")
+#' query_string = spq_init() %>%
+#'   spq_add("?city wdt:P31/wdt:P279* wd:Q486972") %>%
+#'   spq_label(city) %>%
+#'   spq_mutate(coords = wdt::P625(city),
+#'           .within_distance=list(center=c(long=4.84,lat=45.76),
+#'                                radius=5)) %>%
+#'   spq_perform() %>%
+#'   transform_wikidata_coords("coords")
 #' }
-transform_wikidata_coords=function(data,coord_column, prefix=""){
-  f=function(x){
-    if(is.na(x[1])){x=c(NA,NA)}
+transform_wikidata_coords = function(data,coord_column, prefix="") {
+  handle_nas = function(x){
+    if (is.na(x[1])) {
+      x = c(NA,NA)
+    }
     return(x)
   }
-  result=data[[coord_column]] %>%
-    str_extract("(?<=Point\\()[\\d\\.\\-\\s]*") %>%
-    str_split(" ") %>%
-    purrr::map(f)
 
-  lat=result %>%
-    purrr::map_chr(purrr::pluck,2) %>%
+  result = data[[coord_column]] %>%
+    str_extract_all("(?<=Point\\()[\\d\\.\\-\\s]*") %>%
+    strsplit(" ") %>%
+    purrr::map(handle_nas)
+
+  lat = result %>%
+    purrr::map_chr(purrr::pluck, 2) %>%
     as.numeric()
-  lng=result %>%
-    purrr::map_chr(purrr::pluck,1) %>%
+  lng = result %>%
+    purrr::map_chr(purrr::pluck, 1) %>%
     as.numeric()
-  tib_coords=tibble::tibble(lat=lat,
-                    lng=lng) %>%
+  tib_coords = tibble::tibble(lat=lat, lng=lng) %>%
     dplyr::rename_with(.fn = ~ paste0(prefix, .x))
-  tib_result=data %>%
+
+  data %>%
     dplyr::select(-!!coord_column) %>%
     dplyr::bind_cols(tib_coords)
-  return(tib_result)
 }
