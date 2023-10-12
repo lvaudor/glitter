f1fec23983fc5fc4a2e4795fbe351090100e143a
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-10-12 13:52:38 +0200

    fix example

--- R/spq_select.R
+++ R/spq_select.R
@@ -6,8 +6,19 @@
 #' @export
 #' @examples
 #'
-#' query = spq_init()
-#' spq_select(query, count = n (human), eyecolor_label, haircolor_label)
+#' spq_init() |>
+#'   spq_prefix(prefixes = c(dct = "http://purl.org/dc/terms/")) |>
+#'   spq_add(spq('?lexemeId dct:language wd:Q1860')) |>
+#'   spq_mutate(lemma = wikibase::lemma(lexemeId)) |>
+#'   spq_filter(str_detect(lemma, '^pota.*')) |>
+#'   spq_select(- lemma)
+#'
+#' spq_init() |>
+#'   spq_prefix(prefixes = c(dct = "http://purl.org/dc/terms/")) |>
+#'   spq_add(spq('?lexemeId dct:language wd:Q1860')) |>
+#'   spq_mutate(lemma = wikibase::lemma(lexemeId)) |>
+#'   spq_filter(str_detect(lemma, '^pota.*')) |>
+#'   spq_select(lemma)
 spq_select = function(.query = NULL, ..., .spq_duplicate = NULL){
   if (!is.null(.spq_duplicate)) {
     original_spq_duplicate = .spq_duplicate
